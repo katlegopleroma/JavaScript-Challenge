dbda6f05b76c21fdc373ed58a0074b86e493e908
katlegopleroma <51974419+katlegopleroma@users.noreply.github.com>
2019-07-04 03:21:26 +0200

    Add files via upload

---
+++ Javascript-challenge/script.js
@@ -0,0 +1,22 @@
+function  changeColor()
+{
+    var x = document.getElementById("color");
+    x.style.backgroundColor= "blue";
+    document.getElementById("demo").innerHTML = "hi, my name is Blue <br><br><br>392";
+    
+};
+
+function  changeColor1()
+{
+    var x = document.getElementById("color");
+    x.style.backgroundColor= " green";
+    document.getElementById("demo").innerHTML = "hi, my name is Green <br><br><br>497";
+    
+};
+function  changeColor2()
+{
+    var x = document.getElementById("color");
+    x.style.backgroundColor= "red";
+    document.getElementById("demo").innerHTML = "hi, my name is Red<br><br><br>283";
+    
+};  
